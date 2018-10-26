810e4a8bcd06a7a5d4137654b0cae25bd123a645
Gaurav Goel <gaurav.goel167@gmail.com>
2018-10-26 23:08:11 +0530

    Added ;login and payment validation on server side. TODO: call ML functions to validate

--- backend/ipi/views.py
+++ backend/ipi/views.py
@@ -3,6 +3,27 @@ from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
 
 def val_payment(request):
 	if request.method == "POST":
-		
+		data = request.POST
+		transaction = data['transaction']
+		# call ML pay validation function for transaction validation. Set value to "val"
+		val = True
+		if val:
+			return JsonResponse({'status':'success', 'access':True})
+		else:
+			return JsonResponse({'status':'success', 'access':False})
+	else:
+		return HttpResponseNotAllowed(['POST'])
+
+def login_user(request):
+	if request.method == "POST":
+		data = request.POST
+		usr_name = data['user']
+		usr_pass = data['pass']
+		# call ML Typing validation function. Set value to "val"
+		val = True
+		if val:
+			return JsonResponse({'status':'success', 'access':True})
+		else:
+			return JsonResponse({'status':'success', 'access':False})
 	else:
 		return HttpResponseNotAllowed(['POST'])
