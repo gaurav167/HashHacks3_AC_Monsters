84b0d9b63d43f400f198e8b045cb95a6490e4e10
Gaurav Goel <gaurav.goel167@gmail.com>
2018-10-27 02:39:47 +0530

    Train typing model on user credentials

--- backend/ipi/urls.py
+++ backend/ipi/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	url(r'/login/$', views.login_user, name="login"),
 	url(r'/pay/$', views.val_payment, name="validate_payment"),
+	url(r'/train/$', views.gen_typing_data),
 ]
--- backend/ipi/views.py
+++ backend/ipi/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
+import json
+import statistics
 
 @csrf_exempt
 def val_payment(request):
@@ -18,14 +21,62 @@ def val_payment(request):
 @csrf_exempt
 def login_user(request):
 	if request.method == "POST":
-		data = request.POST
-		usr_name = data['user']
-		usr_pass = data['pass']
+		d = request.POST
+		data = None
+		for key in d.keys():
+			data = json.loads(key)
+		# print(dict(data))
+		usr_name = json.loads(data['user'])
+		usr_pass = json.loads(data['pass'])
+		print(usr_name)
+		user = [(chr(i['character']), i['time']) for i in usr_name]
+		password = [(chr(i['character']), i['time']) for i in usr_pass]
+		# print(user)
+		# print(password)
 		# call ML Typing validation function. Set value to "val"
 		val = True
 		if val:
 			return JsonResponse({'status':'success', 'access':'True'})
 		else:
 			return JsonResponse({'status':'success', 'access':'False'})
+	else:
+		return HttpResponseNotAllowed(['POST'])
+
+
+counter = 0
+l = []
+p = []
+@csrf_exempt
+def gen_typing_data(request):
+	global counter
+	if request.method == "POST":
+		d = request.POST
+		data = None
+		for key in d.keys():
+			data = json.loads(key)
+		# print(dict(data))
+		usr_name = json.loads(data['user'])
+		usr_pass = json.loads(data['pass'])
+		print(usr_name)
+		user = [(chr(i['character']), i['time']) for i in usr_name]
+		password = [(chr(i['character']), i['time']) for i in usr_pass]
+		if counter==0:
+			for i in range(len(user)):
+				l.append([])
+			for i in range(len(password)):
+				l.append([])
+
+		for ur in range(len(user)):
+			l[ur].append(user[ur][1])
+		for pr in range(len(password)):
+			p[pr].append(password[ir][1])
+
+		counter+=1
+		if counter == 8:
+			counter = 0
+			# Call training function ML
+			print(l)
+			print(p)
+		print(counter)
 	else:
 		return HttpResponseNotAllowed(['POST'])
