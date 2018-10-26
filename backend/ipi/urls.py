5e032faf9c475bfe9b8059e2eca8c1f4123b1edd
Gaurav Goel <gaurav.goel167@gmail.com>
2018-10-26 23:26:39 +0530

    Fixed backend urls

--- backend/ipi/urls.py
+++ backend/ipi/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
-	url(r'/pay', views.val_payment, name="validate_payment"),
+	url(r'/login/$', views.login_user, name="login"),
+	url(r'/pay/$', views.val_payment, name="validate_payment"),
 ]
